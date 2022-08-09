5b93ad5b67d608c2eb805a41277fda65ceb52e8a
Kensya <himemusou@gmail.com>
2022-08-09 20:41:21 +0300

    head-requests. fix

--- server.js
+++ server.js
@@ -6,23 +6,34 @@ const host = 'localhost';
 const port = 8080;
 
 const requestListener = (req, res) => {
-  if (req.url === '/get' && req.method === 'GET') {
-    fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Internal server error');
-      } else {
-        res.writeHead(200);
-        res.end(files.join(', '));
-      }
-    });
-  } else if ((req.url === '/delete' && req.method === 'DELETE') ||
-             (req.url === '/post' && req.method === 'POST')) {
+  if (req.method === 'GET') {
+    if (req.url === '/get') {
+      fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+        if (err) {
+          res.writeHead(500);
+          res.end('Internal server error');
+        } else {
+          res.writeHead(200);
+          res.end(files.join(', '));
+        }
+      });
+    }
+    if (req.url === '/redirect') {
+      res.writeHead(301, {'Location': `http://${host}:${port}/redirected`});
+      res.end(`Resourse is now permanently available at http://${host}:${port}/redirected`);
+    }
+    if (req.url === '/') {
+      res.writeHead(404);
+      res.end('Route not found');
+    }
+  } else if (req.method === 'POST' || req.method === 'DELETE') {
+    if (req.method === req.url.slice(1).toUpperCase()) {
       res.writeHead(200);
       res.end('Success');
-  } else if (req.url === '/redirect' && req.method === 'GET') {
-    res.writeHead(301);
-    res.end(`Resourse is now permanently available at http://${host}:${port}/redirected`);
+    } else {
+      res.writeHead(405);
+      res.end('HTTP method not allowed');
+    }
   } else {
     res.writeHead(405);
     res.end('HTTP method not allowed');
